6678d4666995f7d7348e0b262963d41af216ee7d
barsbold-coding <barsbold.coding@gmail.com>
2025-05-22 14:19:25 +0800

    Initial Commit

---
+++ SimpleLiDARAppTests/SimpleLiDARAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLiDARAppTests.swift
+//  SimpleLiDARAppTests
+//
+//  Created by Barsbold Bayarerdene on 22/05/2025.
+//
+
+import Testing
+@testable import SimpleLiDARApp
+
+struct SimpleLiDARAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
